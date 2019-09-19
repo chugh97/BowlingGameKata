68d009472668eb31d6d64f33a6a4a60b591e84b5
Shaleen Chugh <shaleen.chugh@nhs.net>
2019-09-19 10:31:17 +0100

    initial commit

---
+++ BowlingGame/BowlingGame.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace BowlingGame
+{
+    public class BowlingGame
+    {
+        private int[] rolls = new int[21];
+        private int currentroll = 0;
+        public void Roll(int p)
+        {
+            rolls[currentroll++] = p;
+        }
+
+        public object Score()
+        {
+            int score = 0;
+            int rollindex = 0;
+            for (int frames = 0; frames < 10; frames++)
+            {
+                if (isStrike(rollindex))
+                {
+                    score += 10 + strikeBonus(rollindex);
+                    rollindex++;
+                }
+                else if (isSpare(rollindex))
+                {
+                    score += 10 + spareBonus(rollindex);
+                    rollindex += 2;
+                }
+                else
+                {
+                    score += rolls[rollindex] + rolls[rollindex+1];
+                    rollindex += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private bool isStrike(int rollindex)
+        {
+            return rolls[rollindex] == 10;
+        }
+
+        private int strikeBonus(int rollindex)
+        {
+            return rolls[rollindex + 1] + rolls[rollindex + 2];
+        }
+
+        private int spareBonus(int rollindex)
+        {
+            return rolls[rollindex + 2];
+        }
+
+        private bool isSpare(int rollindex)
+        {
+            return rolls[rollindex] + rolls[rollindex + 1] == 10;
+        }
+    }
+}
---
+++ BowlingGameUnitTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingGame = BowlingGame.BowlingGame;
+
+namespace BowlingGameUnitTests
+{
+    [TestClass]
+    public class BowlingGameUnitTests
+    {
+        private global::BowlingGame.BowlingGame sut;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            sut = new global::BowlingGame.BowlingGame();
+        }
+
+        [TestMethod]
+        public void TestGutterGame_Should_Return_Zero()
+        {
+            int rolls = 10;
+            int pinsknocked = 0;
+
+            RollMultiplePins(rolls, pinsknocked);
+
+            Assert.AreEqual(0, sut.Score());
+        }
+
+        [TestMethod]
+        public void Test_all_Ones_Return_20()
+        {
+            int rolls = 20;
+            int pinsknocked = 1;
+
+            RollMultiplePins(rolls, pinsknocked);
+
+            Assert.AreEqual(20, sut.Score());
+        }
+
+        void RollMultiplePins(int rolls, int pinsknocked)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                sut.Roll(pinsknocked);
+            }
+        }
+
+        [TestMethod]
+        public void Test_One_Spare()
+        {
+            sut.Roll(5);
+            sut.Roll(5);
+            sut.Roll(3);
+
+            RollMultiplePins(17, 0);
+
+            Assert.AreEqual(16, sut.Score());
+        }
+
+        [TestMethod]
+        public void Test_One_Strike()
+        {
+            rollStrike();
+            sut.Roll(4);
+            sut.Roll(5);
+
+            RollMultiplePins(16, 0);
+
+            Assert.AreEqual(28, sut.Score());
+        }
+
+        [TestMethod]
+        public void Test_PerfectGame()
+        {
+            RollMultiplePins(12, 10);
+
+            Assert.AreEqual(300, sut.Score());
+        }
+
+        private void rollStrike()
+        {
+            sut.Roll(10);
+        }
+    }
+
+
+
+
+}
